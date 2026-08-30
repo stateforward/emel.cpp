25f802b911b9a6bdd890701632f844d7d7544424
EMEL Worker <worker@emel.cpp>
2026-08-30 03:21:41 +0000

    wip(cact): loader events/errors/context

---
+++ src/emel/cact/loader/context.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <span>
+
+#include "emel/cact/loader/events.hpp"
+
+namespace emel::cact::loader::action {
+
+struct context {
+  geometry probed = {};
+  std::span<tensor_view> tensors = {};
+};
+
+}  // namespace emel::cact::loader::action
